9d6fe1943f82035266915accf9e938aef532623a
Vasya <vasilgor@rambler.ru>
2022-07-25 15:34:14 +0300

    3try

--- validate/validate.js
+++ validate/validate.js
@@ -24,18 +24,16 @@ module.exports.validatorUpdateUser = celebrate({
 
 module.exports.validatoraddFilms = celebrate({
   body: Joi.object().keys({
-    name: Joi.string(),
-    email: Joi.string().required().email(),
-    country: Joi.string(),
-    director: Joi.string(),
+    country: Joi.string().required(),
+    director: Joi.string().required(),
     duration: Joi.number().required(),
     year: Joi.string().required(),
-    description: Joi.string(),
-    image: Joi.string().pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
-    trailerLink: Joi.string().pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
-    nameRU: Joi.string(),
-    nameEN: Joi.string(),
-    thumbnail: Joi.string().pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
+    trailerLink: Joi.string().required().pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+    thumbnail: Joi.string().required().pattern(/^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-.~:/?#[\]@!$&'()*+,;=]{2,}#?$/),
     movieId: Joi.number().required(),
   }),
 });
